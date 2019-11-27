c9b4defe27f4738611500e2b47665a2a4debc1c6
tmatuszewski97 <tomasz.matuszewski97@gmail.com>
2019-11-27 23:43:11 +0100

    stworzenie podstawowych serializerów, rozpoczęcie tworzenia widoków

--- my_site/serwis_komputerowy/serializers.py
+++ my_site/serwis_komputerowy/serializers.py
@@ -2,3 +2,32 @@ from rest_framework import serializers
 from .models import Adres, Firma, Klient, Pracownik, Zgloszenie
 
 
+class AdresSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Adres
+        fields = '__all__'
+
+
+class FirmaSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Firma
+        fields = '__all__'
+
+
+class KlientSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Klient
+        fields = '__all__'
+
+
+class PracownikSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Pracownik
+        fields = '__all__'
+        read_only_fields = ['czyZalogowany']
+
+
+class ZgloszenieSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Zgloszenie
+        fields = '__all__'
--- my_site/serwis_komputerowy/views.py
+++ my_site/serwis_komputerowy/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Adres, Firma, Klient, Pracownik, Zgloszenie
+from . import serializers
+from rest_framework import generics
+
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("Witaj, znajdujesz sie w indeksie serwisu komputerowego.")
+
+
+class getAdres (generics.ListAPIView):
+    queryset = Adres.objects.all()
+    serializer_class = serializers.AdresSerializer
+
+
