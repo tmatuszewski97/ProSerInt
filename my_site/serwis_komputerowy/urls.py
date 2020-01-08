5bfabb9aeb92b44a927b5a459cc8b4fe59766846
tmatuszewski97 <tomasz.matuszewski97@gmail.com>
2020-01-09 00:52:31 +0100

    dodanie nowego serializera, widoków, endpointa

--- my_site/serwis_komputerowy/urls.py
+++ my_site/serwis_komputerowy/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('users/<int:pk>/', views.UserDetail.as_view(), name='UserDetail'),
     path('users/data/', views.DaneUzytkownikaList.as_view(), name='UserData'),
     path('users/data/<int:pk>/', views.DaneUzytkownikaDetail.as_view(), name='UserDataDetail'),
-    path('adressess/', views.AdresList.as_view(), name='AdresList'),
-    path('adressess/<int:pk>/', views.AdresDetail.as_view(), name='AdresDetail'),
+    path('adressess/', views.AdresList.as_view(), name='AdressList'),
+    path('adressess/<int:pk>/', views.AdresDetail.as_view(), name='AdressDetail'),
     path('notifications/', views.ZgloszenieList.as_view(), name='NotificationList'),
     path('notifications/<int:pk>/', views.ZgloszenieDetail.as_view(), name='NotificationDetail'),
 ]
--- my_site/serwis_komputerowy/views.py
+++ my_site/serwis_komputerowy/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth.models import User, Group
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Witaj na stronie serwisu komputerowego!")
 
